727d4eeb3e1d06a4684db6a1d14424b095d85f45
Chen Ben Ami <chen24201@gmail.com>
2020-09-11 12:13:25 +0300

    dns

--- log-service/deploy.sh
+++ log-service/deploy.sh
@@ -4,6 +4,7 @@ appName=$(cat config.txt | awk {'print $1'})
 appVersion=$(cat config.txt | awk {'print $2'})
 appPort=$(cat config.txt | awk {'print $3'})
 machinePort=$(cat config.txt | awk {'print $4'})
+machineDns=$(cat config.txt | awk {'print $5'})
 
 echo "appName = ${appName}"
 echo "appVersion = ${appVersion}"
@@ -14,4 +15,4 @@ echo "deploying ${appName} as docker container"
 
 docker build --tag ${appName}:${appVersion} .
 
-docker run --name ${appName} -itd ${appName}:${appVersion} -p ${machinePort}:${appPort}
+docker run --name ${appName} --dns ${machineDns} -p ${machinePort}:${appPort} -itd ${appName}:${appVersion} 
