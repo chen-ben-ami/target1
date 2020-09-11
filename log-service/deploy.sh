42c2c6914438d57db5e7758e06048f7aa044a075
Chen Ben Ami <chen24201@gmail.com>
2020-09-11 11:37:30 +0300

    scripts

---
+++ log-service/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+appName=$(cat config.txt | awk {'print $1'})
+appVersion=$(cat config.txt | awk {'print $2'})
+appPort=$(cat config.txt | awk {'print $3'})
+machinePort=$(cat config.txt | awk {'print $4'})
+
+echo "appName = ${appName}"
+echo "appVersion = ${appVersion}"
+echo "appPort = ${appPort}"
+echo "machinePort = ${machinePort}"
+
+echo "deploying ${appName} as docker container"
+
+docker build --tag ${appName}:${appVersion} .
+
+docker run -itd ${appName}:${appVersion} --name ${appName} -p ${machinePort}:${appPort}
