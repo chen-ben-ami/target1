abf9a617b0b7a76b91daa4a4d92b84cf61f29694
Chen Ben Ami <chen24201@gmail.com>
2020-09-11 12:28:34 +0300

    ports

--- shuffle-service/deploy.sh
+++ shuffle-service/deploy.sh
@@ -4,7 +4,6 @@ appName=$(cat config.txt | awk {'print $1'})
 appVersion=$(cat config.txt | awk {'print $2'})
 appPort=$(cat config.txt | awk {'print $3'})
 machinePort=$(cat config.txt | awk {'print $4'})
-machineDns=$(cat config.txt | awk {'print $5'})
 
 echo "appName = ${appName}"
 echo "appVersion = ${appVersion}"
@@ -15,4 +14,4 @@ echo "deploying ${appName} as docker container"
 
 docker build --tag ${appName}:${appVersion} .
 
-docker run --name ${appName} --dns ${machineDns} -p ${machinePort}:${appPort} -itd ${appName}:${appVersion}
+docker run -itd --publish ${machinePort}:${appPort} --name ${appName} ${appName}:${appVersion} 
